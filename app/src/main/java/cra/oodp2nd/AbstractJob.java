1202b3e68d4efebd4f1fdf7a70755428ba99fe2e
Joshua Jung <wbqdinno@gmail.com>
2015-05-16 20:30:37 +0900

    초기 개발 버전 추가

---
+++ app/src/main/java/cra/oodp2nd/AbstractJob.java
@@ -0,0 +1,25 @@
+package cra.oodp2nd;
+
+/**
+ * Created by wbqd on 15. 5. 16..
+ */
+public abstract class AbstractJob implements Job {
+    private String title;
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/cra/oodp2nd/RecordJob.java
@@ -0,0 +1,7 @@
+package cra.oodp2nd;
+
+/**
+ * Created by wbqd on 15. 5. 16..
+ */
+public class RecordJob extends AbstractJob {
+}
