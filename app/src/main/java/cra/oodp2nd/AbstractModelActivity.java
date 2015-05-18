0f95338c46b5632d6b10df5f304f896a493d4d7e
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 10:35:05 +0900

    AbstractModelActivity 구현

--- app/src/main/java/cra/oodp2nd/AbstractModelActivity.java
+++ app/src/main/java/cra/oodp2nd/AbstractModelActivity.java
@@ -4,13 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public abstract class AbstractModelActivity extends Activity {
 
+    protected DatabaseHelper myDBHelper;
+    protected String tableName;
+    protected String createTableQuery;
+//    protected Button saveButton;
+//    protected Button updateButton;
+
+    protected abstract void setTableName();
+    protected abstract void setCreateTableQuery(String tableName);
+    protected abstract void setSaveButton();
+    protected abstract void setUpdateButton();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myDBHelper = DatabaseHelper.getInstance(this);
+        setTableName();
+        setCreateTableQuery(tableName);
     }
 
 }
