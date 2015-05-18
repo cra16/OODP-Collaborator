e136d935c7c8f8a2321872b7841b3c92dce135c6
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 10:21:49 +0900

    TaskViewActivity에 TaskInterface 구현

--- app/src/main/java/cra/oodp2nd/TaskViewActivity.java
+++ app/src/main/java/cra/oodp2nd/TaskViewActivity.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class TaskViewActivity extends AbstractViewActivity {
+public class TaskViewActivity extends AbstractViewActivity implements TaskInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
