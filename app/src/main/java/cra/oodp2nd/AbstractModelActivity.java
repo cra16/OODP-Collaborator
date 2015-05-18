54e1090c86450bce75bd756a2e4df5bbbd3162f7
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 10:34:31 +0900

    setContentView 삭제

--- app/src/main/java/cra/oodp2nd/AbstractModelActivity.java
+++ app/src/main/java/cra/oodp2nd/AbstractModelActivity.java
@@ -11,9 +11,6 @@ public abstract class AbstractModelActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_abstract_model);
-    }
-
     }
 
 }
