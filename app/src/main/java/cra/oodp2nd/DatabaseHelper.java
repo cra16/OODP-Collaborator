216dfbf3ea9e38cec40acba30ff787e0b0c16065
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 13:13:10 +0900

    새로운 DB 헬퍼 적용

--- app/src/main/java/cra/oodp2nd/DatabaseHelper.java
+++ app/src/main/java/cra/oodp2nd/DatabaseHelper.java
@@ -1,65 +1,69 @@
 package cra.oodp2nd;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by wbqd on 15. 5. 16..
- */
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
     static final String DataBase = "Project_DB";
+    private static DatabaseHelper myDBHelper;
 
-
-    public static DatabaseHelper myDBHelper =null;
-    private SQLiteDatabase myDatabase=null;
     public static DatabaseHelper getInstance(Context context)//singleton
     {
-        if(myDBHelper ==null) {
-            myDBHelper = new DatabaseHelper(context);
-        }
+
+        if(myDBHelper ==null)
+            myDBHelper = new DatabaseHelper(context, DataBase, null,1);
 
         return myDBHelper;
     }
 
-    private DatabaseHelper(Context context)
+    private DatabaseHelper(Context context,String name, SQLiteDatabase.CursorFactory factory, int version)
     {
-        myDatabase=context.openOrCreateDatabase(DataBase,context.MODE_PRIVATE,null);
+        super(context,name,factory, version);
     }
 
 
-    public void CreateTable(String SQL)
-    {
-        try{
-            myDatabase.execSQL(SQL);
-        } catch (Exception e) {
-            e.printStackTrace();
+    /*
+        public long QueryInsert(String TableName,ContentValues RowValue)
+        {
+            return myDatabase.insert(TableName,null,RowValue);
         }
 
+        public Cursor QuerySelect(String TableName, String[] columns, String selection, String[] selectionArgs, String GroupBy, String having, String orderBy)
+        {
+            return myDatabase.query(TableName, columns, selection, selectionArgs, GroupBy, having, orderBy);
+        }
+
+        public int QueryUpdate( String TableName, ContentValues UpRowValue,String whereClause, String[] whereArgs)
+        {
+            return myDatabase.update(TableName, UpRowValue, whereClause, whereArgs);
+        }
+        public int Querydelete(String TableName, String whereClause, String[] whereArgs)
+        {
+            return myDatabase.delete(TableName, whereClause, whereArgs);
+        }
+    */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table if not exists table_task (id integer primary key, title text);");
+        db.execSQL("create table if not exists table_schedule(id integer primary key, title text);");
+        db.execSQL("create table if not exists table_meeting_record (id integer primary key, title text) ;");
+        db.execSQL("create table if not exists table_subtask (id integer primary key, titleId integer, title text, " +
+                "clear boolean, state text);");
+        db.execSQL("create table if not exists table_member (id integer primary key, userId text unique, password text);");
+        db.execSQL("create table if not exists table_memberpresented (id integer primary key, recordID integer, title text, userId text);");
+
     }
 
-    public long QueryInsert(String TableName,ContentValues RowValue)
-    {
-        return myDatabase.insert(TableName,null,RowValue);
-    }
-
-    public Cursor QuerySelect(String TableName, String[] columns, String selection, String[] selectionArgs, String GroupBy, String having, String orderBy)
-    {
-        return myDatabase.query(TableName, columns, selection, selectionArgs, GroupBy, having, orderBy);
-    }
-
-    public int QueryUpdate( String TableName, ContentValues UpRowValue,String whereClause, String[] whereArgs)
-    {
-        return myDatabase.update(TableName, UpRowValue, whereClause, whereArgs);
-    }
-    public int QueryDelete(String TableName, String whereClause, String[] whereArgs)
-    {
-        return myDatabase.delete(TableName, whereClause, whereArgs);
-    }
-
-    public void DatabaseDestory()
-    {
-        myDatabase.close();
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS table_task");
+        db.execSQL("DROP TABLE IF EXISTS table_schedule");
+        db.execSQL("DROP TABLE IF EXISTS table_meetingRecordTable");
+        db.execSQL("DROP TABLE IF EXISTS table_subtask");
+        db.execSQL("DROP TABLE IF EXISTS table_member");
+        db.execSQL("DROP TABLE IF EXISTS table_memberpresentedTable");
+        onCreate(db);
     }
 }
