ceb6586e8544efcb6e0b2ae13aebd5b9d3bc59db
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 10:22:37 +0900

    TaskAddActivity 추가

---
+++ app/src/main/java/cra/oodp2nd/TaskAddActivity.java
@@ -0,0 +1,79 @@
+package cra.oodp2nd;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class TaskAddActivity extends AbstractModelActivity implements TaskInterface {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_add);
+        setSaveButton();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_task_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void setTableName() {
+        tableName = "table_task";
+    }
+
+    @Override
+    protected void setCreateTableQuery(String tableName) {
+        createTableQuery = "create table if not exists" +  tableName +" (id integer primary key, title text);";
+    }
+
+    @Override
+    protected void setSaveButton() {
+        Button saveButton = (Button) findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = (EditText) findViewById(R.id.edit_text_task_title);
+                String title = titleEditText.getText().toString();
+                ContentValues addRowValue = new ContentValues();
+
+                addRowValue.put("title", title);
+                myDBHelper.QueryInsert(tableName, addRowValue);
+
+                Intent intent = new Intent(getApplicationContext(), TaskViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void setUpdateButton() {
+
+    }
+
+}
