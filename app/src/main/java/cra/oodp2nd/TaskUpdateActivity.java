ee09625832c5a58b267008f607c336e2003d9279
Joshua Jung <wbqdinno@gmail.com>
2015-05-18 15:30:06 +0900

    Task update 액티비티 추가

--- app/src/main/java/cra/oodp2nd/TaskUpdateActivity.java
+++ app/src/main/java/cra/oodp2nd/TaskUpdateActivity.java
@@ -1,17 +1,31 @@
 package cra.oodp2nd;
 
-import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class TaskUpdateActivity extends Activity {
+public class TaskUpdateActivity extends AbstractModelActivity implements TaskInterface {
+
+    protected int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_add);
+
+        Bundle bundle = getIntent().getExtras();
+        id = bundle.getInt("p_id");
+        getTaskTitle();
+
+        setUpdateButton();
     }
 
     @Override
@@ -35,4 +49,46 @@ public class TaskUpdateActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void setSaveButton() {
+
+    }
+
+    @Override
+    protected void setUpdateButton() {
+        Button updateButton = (Button) findViewById(R.id.button_add_update);
+        updateButton.setText("Modify");
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = (EditText) findViewById(R.id.edit_text_task_title);
+                String title = titleEditText.getText().toString();
+                ContentValues updateRowValue = new ContentValues();
+
+                updateRowValue.put("title", title);
+                sqLiteDatabase.update(TABLE_NAME, updateRowValue, "id=" + id, null);
+
+                Intent intent = new Intent(getApplicationContext(), TaskViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getTaskTitle() {
+        String[] columns = {"title"};
+
+        Cursor result = sqLiteDatabase.query(TABLE_NAME, columns, "id=" + id, null, null, null, null);
+
+        result.moveToFirst();
+        if (result == null)
+            Log.e("error", "result null");
+        String title = result.getString(0);
+
+        EditText titleEditText = (EditText) findViewById(R.id.edit_text_task_title);
+        titleEditText.setText(title);
+
+        result.close();
+    }
+
 }
