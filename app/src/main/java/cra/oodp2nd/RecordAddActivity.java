787e0fd3eaa254d79f1657c4193b3455c1a638e3
Joshua Jung <wbqdinno@gmail.com>
2015-05-19 21:48:48 +0900

    Finish RecordAddActivity

--- app/src/main/java/cra/oodp2nd/RecordAddActivity.java
+++ app/src/main/java/cra/oodp2nd/RecordAddActivity.java
@@ -1,17 +1,47 @@
 package cra.oodp2nd;
 
-import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class RecordAddActivity extends Activity {
+public class RecordAddActivity extends AbstractModelActivity implements RecordInterface {
+
+    @Override
+    protected void setSaveButton() {
+        Button saveButton = (Button) findViewById(R.id.button_add_update);
+        saveButton.setText("Save");
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = (EditText) findViewById(R.id.edit_text_record_title);
+                String title = titleEditText.getText().toString();
+                ContentValues addRowValue = new ContentValues();
+
+                addRowValue.put("title", title);
+                sqLiteDatabase.insert(TABLE_NAME, null, addRowValue) ;
+
+                Intent intent = new Intent(getApplicationContext(), RecordViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void setUpdateButton() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_add);
+        setSaveButton();
     }
 
     @Override
