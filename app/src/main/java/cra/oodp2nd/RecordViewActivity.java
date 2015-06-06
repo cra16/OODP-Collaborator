008a902f508f9facd8e4b3a8c650d379a7d1c64f
현우 <ksks0908@naver.com>
2015-06-07 05:11:59 +0900

    아이디에 따라 정보 보여주는 거 추가

--- app/src/main/java/cra/oodp2nd/RecordViewActivity.java
+++ app/src/main/java/cra/oodp2nd/RecordViewActivity.java
@@ -37,7 +37,7 @@ public class RecordViewActivity extends AbstractViewActivity implements RecordIn
 
     @Override
     protected void selectData(String[] columns) {
-        Cursor result = sqLiteDatabase.query(TABLE_NAME,columns,null,null,null,null,null);
+        Cursor result = sqLiteDatabase.query(TABLE_NAME,columns,"userId="+"\""+userId+"\"",null,null,null,null);
         result.moveToFirst();
         while(!result.isAfterLast()) {
 
